c66cc06d5898a4d400299c9c7539a7f26fec65ac
repetrepet <repet.py@gmail.com>
2025-04-18 20:46:04 +0300

    Create _2_input.py

---
+++ _01_input_output/_2_input.py
@@ -0,0 +1,10 @@
+input() # - ввод информации через консоль
+
+name = input('Введите имя: ')
+print('Вы ввели:', name)
+
+age = input('Ваш возраст:')
+input(age)
+
+color = input('Запросить цвет:')
+input(color)
